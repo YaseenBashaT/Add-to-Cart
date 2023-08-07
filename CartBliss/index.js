ae91a92ce333ebbde457c954b846d1004f1db6d8
Thippaluri Yaseen Basha <yasenroycey6@gmail.com>
2023-08-08 00:08:47 +0530

    Add files via upload

--- CartBliss/index.js
+++ CartBliss/index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://yasenroyce-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
